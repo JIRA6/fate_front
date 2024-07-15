97db5472448d9866e5d77516eb0dfac89912bccf
marlboro09 <taebong8989@gmail.com>
2024-07-15 11:59:09 +0900

    feat : main에서 detail가는 경로 추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Route, Routes } from 'react-router-dom';
 import MainPage from './pages/main/MainPage';
 import Member from './pages/member/Member';
 import NotFound from './components/global/NotFound';
+import ProjectDetailPage from './components/ProjectDetailPage';
 import './App.css';
 
 const App = () => {
   return (
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path='/member/:id' element={<Member />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="/member/:id" element={<Member />} />
+        <Route path="/project/:id" element={<ProjectDetailPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
   );
 };
